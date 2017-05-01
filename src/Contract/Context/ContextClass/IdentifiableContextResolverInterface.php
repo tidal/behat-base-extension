e3257cca6c1cce4f77c273d9ab42334a88f59b5e
Timo Michna <timomichna@yahoo.de>
2017-05-01 16:25:55 +0200

    add IdentifiableContextResolverInterface

---
+++ src/Contract/Context/ContextClass/IdentifiableContextResolverInterface.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the behat-base-extension package.
+ * (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tidal\Behat\BaseExtension\Contract\Context\ContextClass;
+
+use Behat\Behat\Context\ContextClass\ClassResolver as ClassResolverInterface;
+use Tidal\Behat\BaseExtension\Contract\Context\ContextDefinitionInterface;
+
+/**
+ * Interface Tidal\Behat\BaseExtension\Contract\Context\ContextClass\IdentifiableContextResolverInterface *
+ */
+interface IdentifiableContextResolverInterface extends ClassResolverInterface
+{
+    /**
+     * @param ContextDefinitionInterface $definition
+     */
+    public function addDefinition(ContextDefinitionInterface $definition);
+
+    /**
+     * @param ContextDefinitionInterface $definition
+     * @return bool
+     */
+    public function hasDefinition(ContextDefinitionInterface $definition) : bool;
+
+    /**
+     * @param string $definitionId
+     * @return bool
+     */
+    public function hasDefinitionId(string $definitionId) : bool;
+
+    /**
+     * @param string $definitionId
+     * @return ContextDefinitionInterface
+     */
+    public function getDefinition(string $definitionId) : ContextDefinitionInterface;
+}
+

f61c28544c4a9e37014ccf578550eda7698a2a58
Timo Michna <timomichna@yahoo.de>
2017-05-01 17:40:04 +0200

    add IdentifiableContextInterface

---
+++ src/Contract/Context/IdentifiableContextInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the behat-base-extension package.
+ * (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tidal\Behat\BaseExtension\Contract\Context;
+
+use Behat\Behat\Context\Context as ContextInterface;
+
+/**
+ * Interface Tidal\Behat\BaseExtension\Contract\Context\IdentifiableContextInterface *
+ */
+interface IdentifiableContextInterface extends ContextInterface, ContextDefinitionInterface
+{
+
+}

6df85d7a6fa3fcb20da7ebb78bdbbe00a45a8457
Timo Michna <timomichna@yahoo.de>
2017-05-01 16:10:27 +0200

    add ContextDefinitionException

---
+++ src/Context/ContextDefinitionException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the behat-base-extension package.
+ * (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tidal\Behat\BaseExtension\Context;
+
+/**
+ * Class Tidal\Behat\BaseExtension\Context\ContextDefinitionException *
+ */
+class ContextDefinitionException extends \RuntimeException
+{
+
+}
